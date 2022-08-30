2efffee5fb4185ff2acc075765e8ef0cc1f8c28c
Kunallohakare <Kunallohakare92@gmail.com>
2022-08-30 21:04:14 +0530

    auth2

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,12 +1,33 @@
-const authenticate = function(req, req, next) {
-    //check the token in request header
-    //validate this token
+const jwt =require("jsonwebtoken");
 
+
+
+const validatetoken= function(req,res,next){
+    let token =req.headers['x-Auth-token'];
+
+
+    if (!token)
+    return
+    res.send({status :false,msg:"token must be present"})
+    console.log(token);
+
+
+
+    let decodedToken=jwt.varify(token,"kunal-lohakare");
+    if(!decodedToken){
+        return
+        res.send ({status:false ,msg:"token is invalid"});
+    }
+    req.loggedInUser=decodedToken.userId
     next()
 }
 
-
-const authorise = function(req, res, next) {
-    // comapre the logged in user's id and the id in request
+const checkIfAuthorized=function(req,res,next){
+    let requestedUserId = req.params.userId
+    if(requestedUserId !== req.loggedInUser) {
+        return res.send({status:false,msg:"permission denied"})
+    }
     next()
 }
+module.exports.validatetoken=validatetoken
+module.exports.checkIfAuthorized=checkIfAuthorized
